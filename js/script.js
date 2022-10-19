0a08a100e928ef9f943ad29fdf21de2078aa0975
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2022-10-19 09:28:45 +0530

    added eventListner for sumbit button

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 const quizData = [
     {
-        question:'Which language runs ina web Browser?',
+        question: 'Which language runs in a web Browser?',
         a: 'Java',
         b: 'C',
         c: 'Python',
         d: 'JavaScript',
-        correct: 'd'
+        correct: 'd',
     },
 
     {
-        question:'What does CSS stand for?',
+        question: 'What does CSS stand for?',
         a: 'Central Style Sheets',
         b: 'Cascading Style Sheet',
         c: 'Cascading Simple Sheets',
         d: 'Cars SUVs Sailboats',
-        correct: 'b'
+        correct: 'b',
     },
 
     {
-        question:'What does HTML stand for?',
+        question: 'What does HTML stand for?',
         a: 'Hypertext Markup Language',
         b: 'Hypertext Markdown Language',
         c: 'Hyperloop Machine Language',
         d: 'Helicopter Terminals Motorboats Lamborghinis',
-        correct: 'a'
+        correct: 'a',
     },
 
     {
-        question:'What year Java Script launched?',
+        question: 'What year Java Script launched?',
         a: '1996',
         b: '1995',
         c: '1994',
         d: 'None of above',
-        correct: 'b'
+        correct: 'b',
     }
 
 
@@ -41,32 +41,58 @@ const quizData = [
 const quiz = document.getElementById('quiz');
 const answerEls = document.querySelectorAll('.answer');
 
-const questionE1 = document.getElementById('question');
+const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
-const submit = document.getElementById('submit');
+const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
-let score =0;
-
+let score = 0;
 loadQuiz();
 
 //load all quiz
-function loadQuiz(){
+function loadQuiz() {
     deselectAnswers();
 
     const currentQuizData = quizData[currentQuiz];
 
-    questionE1.innerText=currentQuizData.question;
+    questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
 }
 
-function deselectAnswers(){
-
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
 }
+
+
+function getSelected() {
+    let answer;
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+        return answer;
+    })
+}
+
+submitBtn.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answerd ${score}/${quizData.length} questions correctly</h2> <button onclick="location.reload()">Reload</button>`;
+        }
+    }
+});
 
