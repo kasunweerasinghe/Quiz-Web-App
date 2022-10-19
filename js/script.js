83c65c10f62089e77835a7f5ea17acb1768fd945
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2022-10-19 09:32:22 +0530

    content update

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
+
 const quizData = [
     {
-        question: 'Which language runs in a web Browser?',
-        a: 'Java',
-        b: 'C',
-        c: 'Python',
-        d: 'JavaScript',
-        correct: 'd',
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
     },
 
     {
-        question: 'What does CSS stand for?',
-        a: 'Central Style Sheets',
-        b: 'Cascading Style Sheet',
-        c: 'Cascading Simple Sheets',
-        d: 'Cars SUVs Sailboats',
-        correct: 'b',
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
     },
 
     {
-        question: 'What does HTML stand for?',
-        a: 'Hypertext Markup Language',
-        b: 'Hypertext Markdown Language',
-        c: 'Hyperloop Machine Language',
-        d: 'Helicopter Terminals Motorboats Lamborghinis',
-        correct: 'a',
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
     },
 
     {
-        question: 'What year Java Script launched?',
-        a: '1996',
-        b: '1995',
-        c: '1994',
-        d: 'None of above',
-        correct: 'b',
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
     }
 
 
@@ -76,22 +77,21 @@ function getSelected() {
         if (answerEl.checked) {
             answer = answerEl.id;
         }
-        return answer;
-    })
+    });
+    return answer;
 }
 
-submitBtn.addEventListener("click", () => {
-    const answer = getSelected();
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
     if (answer) {
         if (answer === quizData[currentQuiz].correct) {
-            score++;
+            score++
         }
-        currentQuiz++;
-
+        currentQuiz++
         if (currentQuiz < quizData.length) {
-            loadQuiz();
+            loadQuiz()
         } else {
-            quiz.innerHTML = `<h2>You answerd ${score}/${quizData.length} questions correctly</h2> <button onclick="location.reload()">Reload</button>`;
+            quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correctly</h2> <button onclick="location.reload()">Reload</button>`
         }
     }
 });
