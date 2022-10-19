5a67f18a3b82243219eb785e25bf722e77b01e05
Kasun-Weerasinghe <kasunrachintha2018@gmail.com>
2022-10-19 08:55:32 +0530

    added 2 questions in js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const quizData = [
+    {
+        question:'Which language runs ina web Browser?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    },
+
+    {
+        question:'What does CSS stand for?',
+        a: 'Central Style Sheets',
+        b: 'Cascading Style Sheet',
+        c: 'Cascading Simple Sheets',
+        d: 'Cars SUVs Sailboats',
+        correct: 'b'
+    }
+]
